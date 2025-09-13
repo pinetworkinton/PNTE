6aa408fd6f935968c568d54a84d774f40895cf99
pinetworkinton <232062556+pinetworkinton@users.noreply.github.com>
2025-09-13 16:34:53 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-Share this link on Twitter or YouTube or Zealy to earn 500 PNTE for each referral!
+Share link on Twitter or YouTube or Zealy to earn 500 PNTE for each referral!
+
+https://pinetworkinton.free.nf/airdrop.php
 
   © 2025 PNTE Airdrop. All rights reserved.
 
